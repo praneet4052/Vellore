1b4b83b37dd64a4d15a6509679c9ba4f11a85f8c
praneet4052 <praneetpraneet2000@gmail.com>
2022-03-16 22:48:02 +0530

    junit

--- src/org/all/LanguageClass.java
+++ src/org/all/LanguageClass.java
@@ -5,6 +5,7 @@ import org.telugu.Telugu;
 public class LanguageClass extends Telugu {
 	private void allLanguage() {
 		System.out.println("All");
+		System.out.println("All");
 	}
 public static void main(String[] args) {
 	LanguageClass l=new LanguageClass();
